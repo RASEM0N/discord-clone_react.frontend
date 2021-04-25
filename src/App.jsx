65959b4ea40b858004b10d9bab5d1de5d65ec92b
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-04-25 13:21:56 +0500

    InitialState + Sibebar болванка

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sidebar from './component/Sidebar'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            {/*Sibebar*/}
+            <Sidebar />
+            {/*Chat*/}
+        </div>
+    )
+}
+
+export default App
